67ef5314c01a4add540ec231c7da9fcec68d1a73
clarisseballesterosbscpe1-4 <clarisseballesteros08@gmail.com>
2026-03-11 00:12:56 +0800

    i used float to convert it in decimal

--- bigger_number.py
+++ bigger_number.py
@@ -1,3 +1,9 @@
 #input 2 numbers
-number_1 = input("Enter the first number: ")
-number_2 = input("Enter the second number: ")
+number_1 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the second number: "))
+#print bigger number
+if number_1 > number_2:
+    print(number_1)
+else:
+    print(number_2)
+
