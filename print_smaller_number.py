b68be1b8ddd7d6aa8d8d22865307b913f3be25b9
clarisseballesterosbscpe1-4 <clarisseballesteros08@gmail.com>
2026-03-12 00:09:14 +0800

    initial commit

---
+++ print_smaller_number.py
@@ -0,0 +1,8 @@
+#ask user to input 2 numbers
+num_1 = float(input("Enter a number: "))
+num_2 = float(input("Enter another number: "))
+#check smaller number
+if num_1 < num_2:
+    print(num_1)
+else:
+    print(num_2)
