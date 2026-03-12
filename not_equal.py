f27f203888884623276dd5311b9a2cb3f5a5789a
clarisseballesterosbscpe1-4 <clarisseballesteros08@gmail.com>
2026-03-12 18:31:59 +0800

    initial commit

---
+++ not_equal.py
@@ -0,0 +1,8 @@
+#input 2 number
+num_1 = float(input("Enter the first number: "))
+num_2 = float(input("Enter the second number: "))
+#check number
+if num_1 != num_2:
+    print("The numbers are not equal")
+else:
+    print("The numbers are equal")
