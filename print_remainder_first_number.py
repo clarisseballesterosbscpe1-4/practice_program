8c322e1458cb6ae11ffcffdc23b4c9e7747059f1
clarisseballesterosbscpe1-4 <clarisseballesteros08@gmail.com>
2026-03-18 16:48:02 +0800

    i put // to divide frist num

--- print_remainder_first_number.py
+++ print_remainder_first_number.py
@@ -2,5 +2,7 @@
 num_1 = int(input("Enter first number: "))
 num_2 = int(input("Enter second number: "))
 #check number
+num_1 = num_1 // num_2
 remainder = num_1 % num_2
-print(remainder)
+
+print("Result:", num_1, "Remainder: ",  remainder)
